941a36829c1308e4e248ab055ce190cd34edd68b
peterztan <pt072093@gmail.com>
2019-07-23 17:44:01 -0500

    sectioning glass effect

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDJuG7ed_9K10-C8v44SSt7M8hUtroI-6M",
+  authDomain: "train-schedule-app-6c866.firebaseapp.com",
+  databaseURL: "https://train-schedule-app-6c866.firebaseio.com",
+  projectId: "train-schedule-app-6c866",
+  storageBucket: "",
+  messagingSenderId: "568667441985",
+  appId: "1:568667441985:web:d8f2f47a9816dca8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
